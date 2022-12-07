917bed50af69ab918b7e680c3cfe7b983e001964
JavedKhan2003 <118762504+JavedKhan2003@users.noreply.github.com>
2022-12-07 15:09:39 +0530

    Update gamecode.py

--- gamecode.py
+++ gamecode.py
@@ -7,5 +7,5 @@ def findMissingNumbers(n):
             output.append(i)
     return output
     
-listOfNumbers = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16]
+listOfNumbers = [1, 2, 3, 5, 6, 7, 8, 9, 11, 13, 14, 16]
 print(findMissingNumbers(listOfNumbers))
